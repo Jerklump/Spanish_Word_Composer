b8c14b14f489a67172fe7bfb8b25a0056db0948d
Jerklump <35908342+Jerklump@users.noreply.github.com>
2018-12-01 11:13:07 -0600

    Add files via upload

--- SpanishII.py
+++ SpanishII.py
@@ -4,23 +4,66 @@
 import random
 
 subject = ["Yo","Tu","El","Ella","Ellos","Ellas","Nosotros"]
-verbs = ["es", "eres", "estamos","eres","estan"]
-conj = ["guapo","blanco","negro","divertida","feo","cansada"]
+verbs = ["es", "eres", "estamos","estan","estoy"]
+conj = ["guapo/a","alto/a","bajo/a","divertida/o","emfirma/o","cansada/o"]
 
 #word bank
+#---------------------------------------------------------------------------
 
 x = (random.choice(subject))
 x += (" ") + (random.choice(verbs))
 x += (" ") + (random.choice(conj))
 
-#making sentence
+    if x == "Yo" + "estoy" + conj:
+        complete_sentence = True
+    elif x == "Tu" + "eres" + conj:
+        complete_sentence = True
+    elif x == "El" + "es" + conj:
+        complete_sentence = True
+    elif x === "Ella" + "es" + conj:
+        complete_sentence = True
+    elif x == "Ellos" + "es" + conj:
+        complete_sentence = True
+    elif x == "Ellas" + "es" + conj:
+        complete_sentence = True
+    elif x == "Nosotros" + "estamos" + conj:
+        complete_sentence = True
+    else:
 
+
+#---------------------------------------------------------------------------
 print (" ")
 print ("Is the sentence below correct?")
 print (" ")
 print (x)
+print (" ")
+
+
+#stumped
+
+complete_sentence = input("Is it a proper sentence?")
+    if complete_sentence = True:
+
+
+
+
+
+print ("You entered: ", complete_sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #printing sentence
 #For Mr.Ferrer
-
-print ("I'm not able to make it say yes or no yet.")
